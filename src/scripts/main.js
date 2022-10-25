b8a5cc2596de8f4b941e737fe47a37d97a0f9ece
nonso01 <chukwunonsomartin01@gmail.com>
2022-10-25 22:47:02 +0100

    updates added some func

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,6 +24,13 @@ function rmEvent(e,t,f){
  const remove= e.removeEventListener(t,f)
 }
 
+function opacity(a,b,i){
+ a.classList.add("invalid")
+ a.value=""
+  b.classList.contains("l-opacity")?b.classList.remove("l-opacity"):0
+    if(i){  b.textContent= Errors[i]}
+
+}
 
 
 function normalize(){
@@ -47,16 +54,19 @@ const Card= {
  cvc: dq(".card__cvc"),
  
  name__error: dq(".card__name__in + .error"),
- num__error: dq(".card__num__in + .error")
+ num__error: dq(".card__num__in + .error"),
+ mm__error: dq(".pack + .error"),
+ cvc__error: dq(".card__expire__in + .error")
  
 }
 
 const Errors= [
- "should not be more than 19 char",
- "should not exceed 22 char",
+ "should not be more than 16 char",
+ "names shouldn't exceed 22 char",
  "only valid ASCII word char",
- "can't be more than 2 char",
+ "Too many char",
  "input field can't be empty",
+ "Valid month/year 🙂"
  ]
 
 /* under development*/
@@ -65,41 +75,92 @@ function writeOnCard(){
  
  const allInput= dqA("input").forEach((e,i)=>{
   
-  event(e,"input",()=>{ // any changes
+  event(e,"input",()=>{ 
+   // any changes
+ 
    switch(i){
     case 0:
+     let _zero= /(\d)|(\W{2})/g
+     
      Card.name.textContent= e.value
+     if(e.value.length>22){
+      
+      opacity(e,Card.name__error,1)
+     }
+     if(_zero.test(e.value)){
+     opacity(e,Card.name__error,2)
+     }
+
+   if(e.value.length===0){
+     let t = "Kenechukwu Amah"
+      Card.name.textContent=t
+    }
+
      break
      
    case 1:
-    Card.num.textContent=e.value
+    let v= e.value
+    let ev= v.slice(0,4) +" " + v.slice(4,8) + " " + v.slice(8,12) + " " + v.slice(12,16)
+    
+    Card.num.textContent=ev
+      
     if(e.value.length > e.maxLength){
-     e.classList.add("invalid")
-     e.value=""
-     Card.num__error.textContent= Errors[0]
+     Card.num__error.textContent= Errors[0] // todo
      
-     Card.num__error.classList.contains("l-opacity")?Card.num__error.classList.remove("l-opacity"):0
+     opacity(e,Card.num__error)
+    } 
+    
+   if(e.value.length===0){
+     let t = "12** **** **** **16"
+      Card.num.textContent=t
     }
-    
-    
-    if(e.value.length===0) Card.num.innerText="00** **** **** **00"
-    
     break
     
   case 2:
+ 
      Card.mm.textContent=e.value
+ 
+   if(e.value.length >2){
+      opacity(e,Card.mm__error,3)
+     }
+   if(e.value.length===0){
+      Card.mm.textContent="09"
+    }
+    if(true) {
+     let _zero= /[0-1][0-9]?/
+     
+    if(!_zero.test(e.value)){opacity(e,Card.mm__error,5)}
+    }
+
    break
    
    case 3:
         Card.yy.textContent=e.value
+      if(e.value.length >2){
+    opacity(e,Card.mm__error,3)
+          }
+      if(e.value.length===0){
+      Card.yy.textContent="22"
+    }
 
+      if(true){
+ let _zero= /[2][0-9]?/
+ !_zero.test(e.value)?opacity(e,Card.mm__error,5):0
+    }
+    
     break
     
   case 4:
       Card.cvc.textContent=e.value
+      
+        if(e.value.length >3){
+       opacity(e,Card.cvc__error,3)
+        }
+    e.value.length===0?Card.cvc.textContent=124:0
+
   break
      default:
-     console.log("error")
+     console.warn("error")
      break
    }
    
@@ -144,7 +205,7 @@ function writeOnCard(){
   event(e,"blur",()=>{ // out of focus
    for(let prop in Card){
     Card[prop].classList.remove("layer")
-    csl("out of focus")
+   // csl("out of focus")
    }
   })
  })
